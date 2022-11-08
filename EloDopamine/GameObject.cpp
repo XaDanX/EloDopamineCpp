bc7c86c544def15861998f19e6ba2d078a34d083
JaviPei <peironej@gmail.com>
2022-11-08 17:57:58 -0300

    Clean Commit

--- EloDopamine/GameObject.cpp
+++
@@ -1,9 +0,0 @@
-#include "GameObject.h"
-#include <iostream>
-
-
-bool GameObject::Load(unsigned int objectAddress, bool deepLoad /* if true load all strings etc. */) {
-    this->address = objectAddress;
-
-    return false;
-}
